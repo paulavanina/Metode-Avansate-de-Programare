d208f514759ba3197b009a8be46ab92d134eb1e3
bejera_paula_vanina <87901953+paulavanina@users.noreply.github.com>
2024-01-08 20:45:39 +0200

    Update map.c

--- map.c
+++ map.c
@@ -5,34 +5,34 @@ elemente aleatorii. Să se afișeze elementele în urmă traversării arborelui 
 #include <stdlib.h>
 struct Nod {
 	int val;
-	struct Nod* stanga;
-	struct Nod* dreapta;
+	struct Nod* st;
+	struct Nod* dr;
 };
 //functie pentru crearea unui nou nod ina arbore
 struct Nod* creareNod(int val) {
 	struct Nod* nod = (struct Nod*)malloc(sizeof(struct Nod));
 	nod->val = val;
-	nod->stanga = nod->dreapta = NULL;
+	nod->st = nod->dr = NULL;
 	return nod;
 }
 void preordine(struct Nod* radacina) {
 	if (radacina != NULL) {
 		printf("%d ", radacina->val);
-		preordine(radacina->stanga);
-		preordine(radacina->dreapta);
+		preordine(radacina->st);
+		preordine(radacina->dr);
 	}
 }
 void inordine(struct Nod* radacina) {
 	if (radacina != NULL) {
-		inordine(radacina->stanga);
+		inordine(radacina->st);
 		printf("%d ", radacina->val);
-		inordine(radacina->dreapta);
+		inordine(radacina->dr);
 	}
 }
 void postordine(struct Nod* radacina) {
 	if (radacina != NULL) {
-		postordine(radacina->stanga);
-		postordine(radacina->dreapta);
+		postordine(radacina->st);
+		postordine(radacina->dr);
 		printf("%d ", radacina->val);
 	}
 }
@@ -40,8 +40,8 @@ int inaltime(struct Nod* radacina) {
 	if (radacina == NULL)
 		return 0;
 	else {
-		int stanga_inaltime = inaltime(radacina->stanga);
-		int dreapta_inaltime = inaltime(radacina->dreapta);
+		int stanga_inaltime = inaltime(radacina->st);
+		int dreapta_inaltime = inaltime(radacina->dr);
 
 		if (stanga_inaltime > dreapta_inaltime)
 			return(stanga_inaltime + 1);
@@ -54,8 +54,8 @@ void afisareNivel(struct Nod* radacina, int nivel) {
 	if (nivel == 1)
 		printf("%d ", radacina->val);
 	else if (nivel > 1) {
-		afisareNivel(radacina->stanga, nivel - 1);
-		afisareNivel(radacina->dreapta, nivel - 1);
+		afisareNivel(radacina->st, nivel - 1);
+		afisareNivel(radacina->dr, nivel - 1);
 	}
 }
 void nivelOrder(struct Nod* radacina) {
@@ -68,12 +68,12 @@ void nivelOrder(struct Nod* radacina) {
 int main() {
 	//crearea arborelui binar ordonat
 	struct Nod* radacina = creareNod(5);
-	radacina->stanga = creareNod(3);
-	radacina->dreapta = creareNod(8);
-	radacina->stanga->stanga = creareNod(1);
-	radacina->stanga->dreapta = creareNod(4);
-	radacina->dreapta->stanga = creareNod(7);
-	radacina->dreapta->dreapta = creareNod(9);
+	radacina->st= creareNod(3);
+	radacina->dr = creareNod(8);
+	radacina->st->st = creareNod(1);
+	radacina->st->dr = creareNod(4);
+	radacina->dr->st = creareNod(7);
+	radacina->dr->dr = creareNod(9);
 
 	printf("Pre-ordine: ");
 	preordine(radacina);
